328b0d3fe9bc8fadc4fc08b2bfc7de45bf21f4b1
Forhad ( Sanemi Bd ) <parvezfarhad72@gmail.com>
2025-08-21 14:34:12 +0600

    Create deploy_bridge.js

---
+++ contracts/contracts/contracts/contracts/scripts/scripts/scripts/deploy_bridge.js
@@ -0,0 +1,11 @@
+async function main() {
+    const Bridge = await ethers.getContractFactory("Bridge");
+    const bridge = await Bridge.deploy();
+    await bridge.deployed();
+    console.log("Bridge deployed to:", bridge.address);
+}
+
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
