ab6a1158f1b33af728a4a219de1f8677e9448129
Forhad ( Sanemi Bd ) <parvezfarhad72@gmail.com>
2025-08-21 14:32:29 +0600

    Create deploy_staking.js

---
+++ contracts/contracts/contracts/contracts/scripts/deploy_staking.js
@@ -0,0 +1,11 @@
+async function main() {
+    const Staking = await ethers.getContractFactory("Staking");
+    const staking = await Staking.deploy();
+    await staking.deployed();
+    console.log("Staking deployed to:", staking.address);
+}
+
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
